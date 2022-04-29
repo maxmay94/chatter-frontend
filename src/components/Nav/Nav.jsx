7ceb1998157b080cff7c98c55950debb346a1bf8
maxmay94 <maxmay94@gmail.com>
2022-04-29 15:08:10 -0400

    able to logout

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,8 +7,17 @@ const Nav = (props) => {
   return (
     <nav className="nav-bar">
       <NavLink id="logo" to='/posts'><img src={LogoDesktop} alt="disco-ball" /></NavLink>
-      <NavLink to='/signup'>Sign Up</NavLink>
-      <NavLink to='/signin'>Sign In</NavLink>
+      {
+        props.user
+        ? <> 
+            <NavLink to='/' onClick={props.handleLogout}>Logout</NavLink>
+          </>
+        : <> 
+            <NavLink to='/signup'>Sign Up</NavLink>
+            <NavLink to='/signin'>Sign In</NavLink>
+          </>
+      }
+
     </nav>
   )
 }
